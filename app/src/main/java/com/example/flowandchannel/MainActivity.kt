10cf35017358b90e0a6308a67512c2c1f2038887
Muhammad Raees Ahmed <muhammadraeesahmed38@gmail.com>
2024-08-24 12:17:24 -0700

    upgrade build file , example of suspend function return single object

--- app/src/main/java/com/example/flowandchannel/MainActivity.kt
+++ app/src/main/java/com/example/flowandchannel/MainActivity.kt
@@ -1,14 +1,42 @@
 package com.example.flowandchannel
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            getUsers().forEach {
+                Log.d(TAG, "User : ${it} ")
+            }
+        }
+
+    }
+
+    suspend fun getUsers(): List<String> {
+        var names = mutableListOf<String>()
+        names.add(getUserName(1))
+        names.add(getUserName(2))
+        names.add(getUserName(3))
+        names.add(getUserName(4))
+        return names
+    }
+
+    private suspend fun getUserName(id: Int): String {
+        delay(1000)
+        return "user${id}"
     }
 }
